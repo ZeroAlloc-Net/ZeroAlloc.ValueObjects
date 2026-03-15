7fb464b946fba204e053168f1d388b7f53aa78a8
Marcel Roozekrans <marcel@roozekrans.nl>
2026-03-15 22:06:51 +0100

    chore: scaffold ZeroAlloc.ValueObjects solution

---
+++ src/ZeroAlloc.ValueObjects.Generator/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ZeroAlloc.ValueObjects.Generator
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/ZeroAlloc.ValueObjects.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ZeroAlloc.ValueObjects.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
